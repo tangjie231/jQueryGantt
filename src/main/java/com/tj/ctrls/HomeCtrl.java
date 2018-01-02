e349e69fafbe1036a6046f38b7340adf1c973e07
tangjie <tangjie@daojia.com>
2018-01-02 20:18:08 +0800

    init spring boot

--- src/main/java/com/tj/ctrls/HomeCtrl.java
+++ src/main/java/com/tj/ctrls/HomeCtrl.java
@@ -1,19 +1,45 @@
 package com.tj.ctrls;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @author:jie.tang on 2018-01-02 15:53
  * desc:
  */
-@RestController
+@Controller
+@RequestMapping("/gantt")
 public class HomeCtrl {
 
     @ResponseBody
-    @RequestMapping("/home")
-    public String getHome(){
-        return "hello";
+    @RequestMapping("/getPlan/{projId}")
+    public String getPlanByPorjId(@PathVariable Long projId){
+        return "{\"tasks\":[\n" +
+                "      {\"id\": -1, \"name\": \"Gantt editor\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 0, \"status\": \"STATUS_ACTIVE\", \"depends\": \"\", \"canWrite\": true, \"start\": 1396994400000, \"duration\": 20, \"end\": 1399586399999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": true},\n" +
+                "      {\"id\": -2, \"name\": \"coding\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 1, \"status\": \"STATUS_ACTIVE\", \"depends\": \"\", \"canWrite\": true, \"start\": 1396994400000, \"duration\": 10, \"end\": 1398203999999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": true},\n" +
+                "      {\"id\": -3, \"name\": \"gantt part\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 2, \"status\": \"STATUS_ACTIVE\", \"depends\": \"\", \"canWrite\": true, \"start\": 1396994400000, \"duration\": 2.5, \"end\": 1397167199999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": false},\n" +
+                "      {\"id\": -4, \"name\": \"editor part\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 2, \"status\": \"STATUS_SUSPENDED\", \"depends\": \"3\", \"canWrite\": true, \"start\": 1397167200000, \"duration\": 4, \"end\": 1397685599999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": false},\n" +
+                "      {\"id\": -5, \"name\": \"testing\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 1, \"status\": \"STATUS_SUSPENDED\", \"depends\": \"2:5\", \"canWrite\": true, \"start\": 1398981600000, \"duration\": 5, \"end\": 1399586399999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": true},\n" +
+                "      {\"id\": -6, \"name\": \"test on safari\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 2, \"status\": \"STATUS_SUSPENDED\", \"depends\": \"\", \"canWrite\": true, \"start\": 1398981600000, \"duration\": 2, \"end\": 1399327199999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": false},\n" +
+                "      {\"id\": -7, \"name\": \"test on ie\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 2, \"status\": \"STATUS_SUSPENDED\", \"depends\": \"6\", \"canWrite\": true, \"start\": 1399327200000, \"duration\": 3, \"end\": 1399586399999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": false},\n" +
+                "      {\"id\": -8, \"name\": \"test on chrome\", \"progress\": 0, \"progressByWorklog\": false, \"relevance\": 0, \"type\": \"\", \"typeId\": \"\", \"description\": \"\", \"code\": \"\", \"level\": 2, \"status\": \"STATUS_SUSPENDED\", \"depends\": \"6\", \"canWrite\": true, \"start\": 1399327200000, \"duration\": 2, \"end\": 1399499999999, \"startIsMilestone\": false, \"endIsMilestone\": false, \"collapsed\": false, \"assigs\": [], \"hasChild\": false}\n" +
+                "    ], \"selectedRow\": 2, \"deletedTaskIds\": [],\n" +
+                "      \"resources\": [\n" +
+                "      {\"id\": \"tmp_1\", \"name\": \"Resource 1\"},\n" +
+                "      {\"id\": \"tmp_2\", \"name\": \"Resource 2\"},\n" +
+                "      {\"id\": \"tmp_3\", \"name\": \"Resource 3\"},\n" +
+                "      {\"id\": \"tmp_4\", \"name\": \"Resource 4\"}\n" +
+                "    ],\n" +
+                "      \"roles\":[\n" +
+                "      {\"id\": \"tmp_1\", \"name\": \"项目经理\"},\n" +
+                "      {\"id\": \"tmp_2\", \"name\": \"RD\"},\n" +
+                "      {\"id\": \"tmp_3\", \"name\": \"FE\"},\n" +
+                "      {\"id\": \"tmp_4\", \"name\": \"QA\"}\n" +
+                "    ], \"canWrite\":    true, \"canDelete\":true, \"canWriteOnParent\": true, \"zoom\": \"w3\"}";
     }
+
+
 }
